5cc7d6f035d456a9d9291c4ad042196e9c0bdb9d
Sanyo4ek952 <Sanyo4ek952@gmail.com>
2025-02-04 15:54:40 +0300

    refactor:  delete navigate

--- src/features/chat/ui/Chat.tsx
+++ src/features/chat/ui/Chat.tsx
@@ -1,22 +1,11 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import styles from './Chat.module.scss';
 import {ChatList} from "./ChatList/ChatList";
 import {ChatWindow} from "./ChatWindow/ChatWindow";
 import {PhoneInput} from "./PhoneInput/PhoneInput";
-import {useNavigate} from "react-router-dom";
-import {storage} from "../../../common/utils/storage";
 
 const Chat = () => {
-    const idInstance = storage.getIdInstance()
-    const apiTokenInstance = storage.getApiTokenInstance()
-    let navigate = useNavigate();
 
-    useEffect(() => {
-        if (idInstance === '' || apiTokenInstance === '') {
-            navigate('/')
-            return
-        }
-    }, [idInstance, apiTokenInstance, navigate]);
     return (
         <div className={styles.container}>
             <div className={styles.leftColumn}>
